37bb84c8c3274f318adc70fc1af67437456edf3e
Iman Ikram <iman.ikram1122@gmail.com>
2024-11-09 21:16:54 -0500

    Styling home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,24 +6,26 @@ The Home component is used to demonstrate the use of Link.
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
+import "../components/Home.css";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <img src="https://picsum.photos/200/200" alt="bank"/>
-
+      <div className="home-page-container">
+        <img src="https://picsum.photos/200/200" alt="bank" />
         <h1>Bank of React</h1>
 
-        <Link to="/userProfile">User Profile</Link>
-        <br/>
-        <Link to="/login">Login</Link>
-        <br/>
-        <Link to="/credits">Credits (to be implemented in the Assignment)</Link>
-        <br/>
-        <Link to="/debits">Debits (to be implemented in the Assignment)</Link>
-        <br/><br/>
-        <AccountBalance accountBalance={this.props.accountBalance}/>
+        <Link to="/userProfile" className="link">User Profile</Link>
+        <br />
+        <Link to="/login" className="link">Login</Link>
+        <br />
+        <Link to="/credits" className="link">Credits</Link>
+        <br />
+        <Link to="/debits" className="link">Debits</Link>
+        
+        <div className="account-balance-container">
+          <AccountBalance accountBalance={this.props.accountBalance} />
+        </div>
       </div>
     );
   }
